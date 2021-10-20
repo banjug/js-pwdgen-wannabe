85dc837062168ce2e9ef055ff0fffd8741eb0bd8
Samuele <miotto.samuele@gmail.com>
2021-10-20 15:22:56 +0200

    complete exercise

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// chiede all'utente il nome
+
+let userName = prompt("Inserisci il tuo nome");
+
+// chiede all'utente il cognome
+
+let userSurname = prompt("Inserisci il tuo cognome");
+
+// chiede all'utente il colore preferito
+
+let userColor = prompt("Inserisci il tuo colore preferito");
+
+// unisce le variabili
+
+let userPassword = userName + userSurname + userColor + "21";
+
+// debug calcolo
+console.log(userPassword);
+
+// mostra il risultato all'utente
+
+document.getElementById('my_container').innerHTML = `La tua password &egrave; ${userPassword}`;
